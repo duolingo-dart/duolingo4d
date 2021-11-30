94bfb28da6e9201f94651faa7494a566249abffa
Kato Shinya <kato.shinya.dev@gmail.com>
2021-12-01 01:04:08 +0900

    add

---
+++ test/duolingo4d_test.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2021, Kato Shinya. All rights reserved.
+// Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('', () {});
+}
