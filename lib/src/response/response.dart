dd836cddc84ac679e8ca9781d539e7837a343f59
Kato Shinya <kato.shinya.dev@gmail.com>
2021-12-01 17:18:29 +0900

    update

--- lib/src/response/response.dart
+++ lib/src/response/response.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Project imports:
 import 'package:duolingo4d/src/response/status.dart';
 
 /// This abstract class represents the common elements
