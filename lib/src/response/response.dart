b3d6d1b254243fb4ed41624e1cc2427f58f8333e
Kato Shinya <kato.shinya.dev@gmail.com>
2021-12-01 17:01:26 +0900

    update

--- lib/src/response/response.dart
+++ lib/src/response/response.dart
@@ -2,7 +2,46 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// The abstract class that represents that it is an HTTP response entity.
+/// This abstract class represents the common elements
+/// of the entity objects corresponding to the Duolingo API.
+///
+/// This abstract class manages the following information.
+/// 1. [statusCode]     ->  HTTP status code included in the response returned by the API.
+/// 2. [reasonPhrase]   ->  Message included in the response returned by the API.
+/// 3. [headers]        ->  Headers included in the response returned by the API.
+///
+/// If you want to check the HTTP status and header information of the response
+/// after calling a method of `Duolingo4D`, you can refer to the entity object
+/// that inherits from this abstract class.
+///
+/// **_Example:_**
+///
+/// ```dart
+/// void main() async {
+///   final duolingo = Duolingo.getInstance();
+///
+///   final authResponse = await duolingo.authenticate(
+///     username: 'test_username',
+///     password: 'test_password',
+///   );
+///
+///   if (authResponse.statusCode != 200) {
+///     authResponse.reasonPhrase;
+///     authResponse.headers;
+///     return;
+///   }
+///
+///   if (authResponse.hasError) {
+///     final authError = authResponse.error!;
+///     print(authError.code);
+///     print(authError.reason);
+///
+///     authError.isInvalidUser;
+///     authError.isInvalidPassword;
+///     return;
+///   }
+/// }
+/// ```
 abstract class Response {
   /// Returns the new instance of [Response] based on arguments.
   Response.from({
