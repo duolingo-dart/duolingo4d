051d0482fa134a67c082ce7c24479bce6e1b975f
Kato Shinya <kato.shinya.dev@gmail.com>
2022-01-05 13:03:51 +0900

    update

--- lib/src/request/stories_request.dart
+++ lib/src/request/stories_request.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // Package imports:
+import 'package:duolingo4d/src/response/stories/stories_response.dart';
 import 'package:http/http.dart' as http;
 
 // Project imports:
